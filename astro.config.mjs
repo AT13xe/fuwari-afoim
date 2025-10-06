580b25cd96c0c5b2ef0144a21bfa371fb745b6cc
AT13xe <845043294@qq.com>
2025-10-06 16:04:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ export default defineConfig({
       image: {
     service: passthroughImageService()
   },
-    site: "https://2x.nz",
+    site: "https://blog.at13xe.top",
     base: "/",
     trailingSlash: "always",
     output: "static",
